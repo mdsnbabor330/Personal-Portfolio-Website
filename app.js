081118f3ef0318536e1c46604eded5cace34dd53
Mohammad Nahid Uddin Babor <mdsnbabor828@gmail.com>
2025-12-07 00:21:29 +0600

    About page 99% complete

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//inspect menu off
 document.addEventListener("contextmenu",function(e){
     e.preventDefault();
+});
+
+//for navbar
+const links = document.querySelectorAll(".navbar a");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    document.querySelector(".navbar a.active")?.classList.remove("active");
+    link.classList.add("active");
+  });
 });
